1cd9bd502b7f038f7b3ae7417f600b8a824e2ba4
Siddhanth Jain <sj6099@srmist.edu.in>
2022-03-07 12:58:39 +0530

    commit 1.0
    Added Header Components

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import '../src/styles/header.css';
 import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton } from '@mui/material';
+import { Avatar,IconButton } from '@mui/material';
+import logo from '../src/images/gmail.png';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 
 function Header(){
-    return (<div className='heading'>
+    return (
+    <div className='heading'>
         <div className="header-left">
             <IconButton>
                 <MenuIcon/>
             </IconButton>
+            <img src={logo} alt="gmail" />
         </div>
         <div className="header-middle">
-            
+            <SearchIcon/>
+            <input placeholder="Search Mail" type="text" />
+            <ArrowDropDownIcon className='Header-input'/>
         </div>
         <div className="header-right">
+            <IconButton>
+                <AppsIcon/> 
+            </IconButton>
+            <IconButton>
+                <NotificationsIcon/>    
+            </IconButton>
+            <Avatar/>
             
         </div>
     </div>)
